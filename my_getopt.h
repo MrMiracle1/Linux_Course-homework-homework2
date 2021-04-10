4592ffd4819ea1e58422a82a002b217e0b06a95d
1243702053@qq.com <1243702053@qq.com>
2021-04-10 12:41:02 +0800

    1.1

--- my_getopt.h
+++ my_getopt.h
@@ -16,14 +16,7 @@ extern "C"{
 extern char* optarg; //当前选项参数字符串
 extern int optind; //argv的当前索引值
 
-struct option{
-    const char *name;
-    int has_arg;
-    int *flag;
-    int val;
-}
-
-int my_getopt(int argc,char *argv[],const char* optstring);
+int my_getopt(int argc,char * const argv[],const char* optstring);
 
 #if defined(__cplusplucs)
 }
